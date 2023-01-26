34c4803f0d7f72c9d921ecd0611cadd77d7ff412
FouadTaissate <112860051+FouadTaissate@users.noreply.github.com>
2023-01-26 13:02:28 +0100

    f

--- script.js
+++ script.js
@@ -27,3 +27,37 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "flex";
 }
+
+// const open = document.getElementById("open");
+// const modal_container = document.getElementById("modal_container");
+// const close = document.getElementById("close");
+
+// open.addEventListener("click", () => {
+//   // modal_container.classList.add("show");
+//   document.getElementsByClassName("modal_container.show").style.display =
+//     "block";
+// });
+
+// close.addEventListener("click", () => {
+//   document.getElementById("show").style.display = "block";
+// });
+
+// function hide() {
+//   document.getElementById("show").style.display = "block";
+// }
+
+// function hide() {
+//   document.getElementById("modal_container").style.display === "none";
+// }
+
+// const open = document.getElementById("open");
+// const modal_container = document.getElementById("modal_container");
+// const close = document.getElementById("close");
+
+// open.addEventListener("click", () => {
+//   modal_container.classList.add("show");
+// });
+
+// close.addEventListener("click", () => {
+//   modal_container.classList.remove("show");
+// });
